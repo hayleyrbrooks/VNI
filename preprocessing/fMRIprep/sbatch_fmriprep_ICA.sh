c027aa82fea4347e77ffb2e6b3022651eb45b262
hayley <hayleynroper@gmail.com>
2021-12-06 15:56:44 -0700

    update script to new node specification
    added notes as to why we are doing this.

--- preprocessing/fMRIprep/sbatch_fmriprep_ICA.sh
+++ preprocessing/fMRIprep/sbatch_fmriprep_ICA.sh
@@ -3,12 +3,17 @@
 #SBATCH --mail-type=ALL     
 #SBATCH --mail-user=hayley.brooks@du.edu  
 #SBATCH --ntasks=1
-#SBATCH --nodelist node[0##]              # Specify node e.g. 003 and use 003-011 since these are not bought by others on RDAC
-#SBATCH --cpus-per-task=8     
+#SBATCH --nodelist=node[0##]              # Specify node e.g. 003 and use 003-011 since these are not bought by others on RDAC
+#SBATCH --cpus-per-task=1     
 #SBATCH --mem=70gb                        # Job memory request
 #SBATCH --time=14-00:00:00                # Time limit dd-hrs:min:sec
 #SBATCH --output=fmriprep_ica%A_%a.log    # Standard output and error log -- %A for parent array and %a for sub array 
-#SBATCH --array=1 # specify which rows we want this script to look for in ids.txt
+#SBATCH --array=19-21 # specify which rows we want this script to look for in ids.txt. 
+
+# Can only do a few participants a time ^^ because we can't spread fmriprep across nodes (which would speed it up) because we run into a reproducibility issue. 
+# So we are allocating these jobs to one node (specified above). One option would be to submit this script and then change the array specifications and run the script again
+# but need to be mindful of how many jobs are submitted on RDAC at once. HRB has tried doing it this way and ran like 20 participants at once and they all got hung up.
+ 
 
 # change directory to VNI head directory
 cd ../
